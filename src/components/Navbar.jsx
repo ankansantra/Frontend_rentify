6bd3af07dd3f32105b22cb534a8ae2efe8446d37
Ankan Santra <123286549+ankansantra@users.noreply.github.com>
2024-05-27 20:53:05 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,104 @@
+import { IconButton } from "@mui/material";
+import { Search, Person, Menu } from "@mui/icons-material";
+import variables from "../styles/variables.scss";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import "../styles/Navbar.scss";
+import { Link, useNavigate } from "react-router-dom";
+import { setLogout } from "../redux/state";
+
+
+const Navbar = () => {
+  const [dropdownMenu, setDropdownMenu] = useState(false);
+
+  const user = useSelector((state) => state.user);
+
+  const dispatch = useDispatch();
+
+  const [search, setSearch] = useState("")
+
+  const navigate = useNavigate()
+
+  return (
+    <div className="navbar">
+      <Link to="/" className="navbar_brand">
+        <p>Rentify</p>
+      </Link>
+
+      <div className="navbar_search">
+        <input
+          type="text"
+          placeholder="Search ..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <IconButton disabled={search === ""}>
+          <Search
+            sx={{ color: variables.pinkred }}
+            onClick={() => {navigate(`/properties/search/${search}`)}}
+          />
+        </IconButton>
+      </div>
+
+      <div className="navbar_right">
+        {user ? (
+          <a href="https://rentify-backend-bi7v.onrender.com/create-listing" className="host">
+            Become A Host
+          </a>
+        ) : (
+          <a href="https://rentify-backend-bi7v.onrender.com/login" className="host">
+            Become A Host
+          </a>
+        )}
+
+        <button
+          className="navbar_right_account"
+          onClick={() => setDropdownMenu(!dropdownMenu)}
+        >
+          <Menu sx={{ color: variables.darkgrey }} />
+          {user && user.profileImagePath && (
+            <img
+              src={`https://rentify-backend-bi7v.onrender.com/${user.profileImagePath.replace(
+                "public",
+                ""
+              )}`}
+              alt=""
+              style={{ objectFit: "cover", borderRadius: "50%" }}
+            />
+          )}
+          {!user && (
+            <Person sx={{ color: variables.darkgrey }} />
+          )}
+        </button>
+
+        {dropdownMenu && !user && (
+          <div className="navbar_right_accountmenu">
+            <Link to="/login">Log In</Link>
+            <Link to="/register">Sign Up</Link>
+          </div>
+        )}
+
+        {dropdownMenu && user && (
+          <div className="navbar_right_accountmenu">
+            <Link to={`/${user._id}/trips`}>Trip List</Link>
+            <Link to={`/${user._id}/wishList`}>Wish List</Link>
+            <Link to={`/${user._id}/properties`}>Property List</Link>
+            <Link to={`/${user._id}/reservations`}>Reservation List</Link>
+            <Link to="/create-listing">Become A Host</Link>
+
+            <Link
+              to="/login"
+              onClick={() => {
+                dispatch(setLogout());
+              }}
+            >
+              Log Out
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
